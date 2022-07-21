4b408d74c88a2bad327b5e3c71767b93adf53ec2
Vouu <contact@mtz.pw>
2022-07-21 17:32:09 +0700

    init

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+function docker_install() {
+    docker_check=$(which docker)
+    if [ -z "$docker_check" ]; then
+        curl -fsSL https://get.docker.com -o get-docker.sh
+        sh get-docker.sh
+    fi
+}
+
+function docker_compose_install() {
+    docker_compose_check=$(which docker-compose)
+    if [ -z "$docker_compose_check" ]; then
+        apt install jq -y || yum install jq -y
+        compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
+        output='/usr/local/bin/docker-compose'
+        curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-$(uname -s)-$(uname -m) -o $output
+        chmod +x $output
+        
+    fi
+}
+
+function sonarqube_docker_pull()
+{
+    docker pull sonarqube:9.5-enterprise
+}
+
+function sonarqube_docker_path_file()
+{
+    mkdir -p /opt/sonarqube/lib
+    cd /opt/sonarqube/lib && wget https://github.com/vncloudsco/random/releases/download/v3/sonar-application-9.5.0.56709.jar
+}
+
+function sonarqube_docker_start()
+{
+    docker-compose up -d
+}
+
+docker_install
+docker_compose_install
+sonarqube_docker_pull
+sonarqube_docker_path_file
+sonarqube_docker_start
+
+echo "Install done check sonarqube on http://$(curl -s ifconfig.io):9000"
